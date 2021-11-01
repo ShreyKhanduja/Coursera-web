0b370e9320ca1ce3b943570c9aeb00579f2fe195
Shrey <shereykhanduja@gmail.com>
2021-11-02 00:15:37 +0530

    names update

--- module 4 assingment/script.js
+++ module 4 assingment/script.js
@@ -1,13 +1,13 @@
 var names=new Array();
-names[0]="Shrey";
-names[1]="Jaadu";
+names[0]="Yaakov";
+names[1]="John";
 names[2]="Jen";
-names[3]="jerry";
-names[4]="yash";
-names[5]="Sanchit";
-names[6]="lovelleen";
-names[7]="Jaspreet";
-names[8]="akshatt";
+names[3]="jason";
+names[4]="paul";
+names[5]="frank";
+names[6]="larry";
+names[7]="paula";
+names[8]="laura";
 names[9]="jim";
 
 
